5bed928b0d07c7a1e5fa0d2cfb27d9695a55e92f
Amanda Barbosa Sobrinho <amandabarbosa@sjrp.unesp.br>
2018-09-25 17:42:21 -0300

    Início da função de ler e parsear arquivos de assinaturas

---
+++ readSignature.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Obter caminho das assinaturas
+sigPath = input("Digite o caminho do arquivo de assinaturas: ")
+
+# Abrir arquivo de assinaturas
+with open(sigPath).read() as sigFile:
+    
+
+
