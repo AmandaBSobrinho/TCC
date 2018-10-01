440d49e9fc6c44278eea2a53f2b9a88e42f17a42
Amanda Barbosa Sobrinho <amandabarbosa@sjrp.unesp.br>
2018-10-01 11:58:21 -0300

    Função que lê e parseia assinaturas retornando dicionários qualitativo/quantitativo

--- read_signature.py
+++ read_signature.py
@@ -6,14 +6,14 @@ def atribute_types():
     with open(sig_path, 'r') as signature_file:
         signature = json.load(signature_file)
 
-    qualitativos = []
-    quantitativos = []
+    qualitativos = {}
+    quantitativos = {}
 
     for step in signature['signature']:
         for atribute in signature['signature'][step]:
             if signature['signature'][step][atribute]['operator'] == 'equal':
-                qualitativos.append(atribute)
+                qualitativos[atribute] = signature['signature'][step][atribute]['value_max']
             if signature['signature'][step][atribute]['operator'] == 'interval':
-                quantitativos.append(atribute)
+                quantitativos[atribute] = signature['signature'][step][atribute]['value']
 
     return qualitativos, quantitativos
