6f06314210ee496248b8d2982229ef0bd4da45ea
Amanda Barbosa Sobrinho <amandabarbosa@sjrp.unesp.br>
2018-10-03 17:34:46 -0300

    Leitura de vários arquivos de assinaturas retornando uma lista com todos os valores (mesmo repetidos)

--- read_signature.py
+++ read_signature.py
@@ -1,19 +1,24 @@
 import json
+from os import listdir
+from os.path import isfile, join
+
 
 def atribute_types():
     sig_path = input("Caminho: ")
-
-    with open(sig_path, 'r') as signature_file:
-        signature = json.load(signature_file)
+    files = [join(sig_path, f) for f in listdir(sig_path) if isfile(join(sig_path, f))]
 
     qualitativos = {}
     quantitativos = {}
 
-    for step in signature['signature']:
-        for atribute in signature['signature'][step]:
-            if signature['signature'][step][atribute]['operator'] == 'equal':
-                qualitativos[atribute] = signature['signature'][step][atribute]['value_max']
-            if signature['signature'][step][atribute]['operator'] == 'interval':
-                quantitativos[atribute] = signature['signature'][step][atribute]['value']
+    for file in files:
+        with open(file, 'r') as signature_file:
+            signature = json.load(signature_file)
+            for step in signature['signature']:
+                for atribute in signature['signature'][step]:
+                    if signature['signature'][step][atribute]['operator'] == 'equal':
+                        qualitativos.setdefault(atribute, []).append(
+                            signature['signature'][step][atribute]['value_max'])
+                    if signature['signature'][step][atribute]['operator'] == 'interval':
+                        quantitativos.setdefault(atribute, []).append(signature['signature'][step][atribute]['value'])
 
     return qualitativos, quantitativos
