56096007f8d7fa6fd1d7a8482f8f50144c2da781
Amanda Barbosa Sobrinho <amandabarbosa@sjrp.unesp.br>
2018-10-21 00:54:46 -0300

    Saídas sendo inseridas no JSON! Agora tem que ler o JSON e determinar a seleção de assinaturas

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import bootstrap
 import normality_test
 import confidence_interval
 import pareto
+import json
 
 
 def main():
@@ -18,18 +19,23 @@ def main():
     quantitativos = {'nBytesSrc': [1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 7, 8, 8, 8, 8, 8, 8, 8, 9, 9, 11, 11, 11, 11, 11, 11, 12, 12, 12,
        12, 12, 12, 13, 13, 14], 'nPortSrc': [1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8, 8, 9, 9]}
 
+    saida_json = {}
+
     # Atributos qualitativos
     for atributo, valores in qualitativos.items():
         atrib_freq = pareto.pareto(atributo, valores)
         print('Valores mais frequentes de ' + atributo + ': ' + ', '.join(str(valor) for valor in atrib_freq))
 
-        # Agora tem que colocar no json
-
+        saida_json[atributo] = {}
+        saida_json[atributo].update({"valor": atrib_freq})
 
     # Para os atributos quantitativos, é preciso fazer o histograma e detectar picos
     for atributo, valores in quantitativos.items():
         histogramas = peak_detection.peak_detection(atributo, valores)
         print('\n' + atributo + ': ' + str(histogramas) + '\n')
+
+        saida_json[atributo] = {}
+        cont = 1
 
         for histograma in histogramas:
 
@@ -49,7 +55,12 @@ def main():
             min_quantile, max_quantile = confidence_interval.calculate(saida_normalizada)
             print('Intervalo de confiança de ' + atributo + ': ' + str(min_quantile) + ' - ' + str(max_quantile))
 
-            # Agora, colocar os atributos mais comuns/intervalos de confiança num json!
+            saida_json[atributo].update({"min{}".format(cont): min_quantile, "max{}".format(cont): max_quantile})
+            cont = cont + 1
+
+    with open('valores.json', 'a') as arquivo:
+        json.dump(saida_json, arquivo, indent=4)
+
 
 if __name__ == "__main__":
     main()
